b887f7e7d27a3925318d4c6c09b7efcea4d5971d
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-08-13 23:36:06 -0400

    adding more strings to regex collection

--- models/models.go
+++ models/models.go
@@ -1,9 +1,8 @@
 package models
 
 type Args struct {
-	File     string `help:"file to scan"`
-	Dir      string `help:"directory with files to scan"`
-	Category string `help:"category of data to scan for"`
-	OutDir   string `help:"name of directory to save results to"`
-	Workers  int    `help:"number of threads for scanning" default:"20"`
+	File    string `help:"file to scan"`
+	Dir     string `help:"directory with files to scan"`
+	OutDir  string `help:"name of directory to save results to"`
+	Workers int    `help:"number of threads for scanning" default:"20"`
 }
