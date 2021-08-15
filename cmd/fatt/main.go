c91e7173259d299018d95b8fa9984302304b51ae
Alexis Rodriguez <rodriguez10011999@gmail.com>
2021-08-14 23:53:06 -0400

    adding concurrency to project

--- cmd/fatt/main.go
+++ cmd/fatt/main.go
@@ -1,14 +1,80 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"os"
+	"regexp"
 
 	"github.com/alexflint/go-arg"
 	"github.com/binexisHATT/fatt/models"
+	"github.com/binexisHATT/fatt/strings"
 )
+
+const (
+	red     = "\u001b[31m"
+	green   = "\u001b[32m"
+	blue    = "\u001b[34m"
+	yellow  = "\u001b[33m"
+	megenta = "\u001b[33m"
+	end     = "\u001b[0m"
+)
+
+var (
+	fileData string
+
+	// Errors
+	FileError = errors.New(fmt.Sprintf("%sFATT%s: unable to open file", red, end))
+)
+
+func readFile(file string, queue chan<- string) {
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Println(FileError)
+		os.Exit(1)
+	}
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		queue <- scanner.Text()
+	}
+	close(queue)
+}
+
+func search(queue <-chan string, finished chan bool) {
+	for line := range queue {
+		if line == "" {
+			continue
+		} else {
+			for patternName, pattern := range strings.Patterns {
+				match, _ := regexp.MatchString(pattern, line)
+				if match {
+					fmt.Println(patternName, match, line)
+				}
+			}
+		}
+	}
+	finished <- true
+}
 
 func main() {
 	c := models.Args{}
 	arg.MustParse(&c)
-	fmt.Printf("%+v", c)
+
+	workQueue := make(chan string)
+	finished := make(chan bool)
+
+	if c.File != "" {
+		go readFile(c.File, workQueue)
+	}
+
+	for i := 0; i < c.Workers; i++ {
+		go search(workQueue, finished)
+	}
+
+	for i := 0; i < c.Workers; i++ {
+		<-finished
+	}
+
+	close(finished)
 }
--- models/models.go
+++ models/models.go
@@ -1,8 +1,7 @@
 package models
 
 type Args struct {
-	File    string `help:"file to scan"`
-	Dir     string `help:"directory with files to scan"`
-	OutDir  string `help:"name of directory to save results to"`
+	File    string `help:"file or directory to scan" arg:"required"`
+	OutDir  string `help:"name of directory to save results to" default:"fatt-out"`
 	Workers int    `help:"number of threads for scanning" default:"20"`
 }
