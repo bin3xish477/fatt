76633577b83e251eb685ba3a620a292ea5bc009b
Alex Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2021-08-17 23:14:13 -0400

    Update models.go

--- models/models.go
+++ models/models.go
@@ -1,8 +1,8 @@
 package models
 
 type Args struct {
-	Url     string `help:"url to scan" arg:"-u,--url"`
 	File    string `help:"file to scan" arg:"-f,--file"`
+	Url     string `help:"url to scan" arg:"-u,--url"`
 	OutFile string `help:"name of directory to save results to" arg:"-o,--outfile"`
 	Workers int    `help:"number of threads for scanning" default:"20" arg:"-w,--workers"`
 	NoColor bool   `help:"turn off color output" arg:"-n,--nocolor"`
